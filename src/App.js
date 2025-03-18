f1b8ac4e71f8e9d9b37ec50121d467e6ffcd8aca
richardpilot <RichardPilot2000@hotmail.com>
2025-03-18 20:23:34 +0000

    Move to sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BodyMap from "./components/BodyMap";
 import ShopPage from "./pages/ShopPage";
-import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
     <>
-      <GlobalStyles />
       <Router>
         <Routes>
           <Route path="/" element={<BodyMap />} />
