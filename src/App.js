136244dd61b0348e8c50477e1facb861534ef852
richardpilot <RichardPilot2000@hotmail.com>
2025-04-19 17:30:28 +0100

    Remove ShopPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BodyMap from "./components/BodyMap";
-import ShopPage from "./pages/ShopPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<BodyMap />} />
-        <Route path="/:part" element={<ShopPage />} />
       </Routes>
     </Router>
   );
