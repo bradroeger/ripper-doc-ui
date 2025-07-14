e0e722d332797fc1aacf438404016efcdaa803d6
richardpilot <RichardPilot2000@hotmail.com>
2025-07-14 23:03:09 +0100

    Fix shop view layout

--- src/components/BodyMap.js
+++ src/components/BodyMap.js
@@ -1,36 +1,36 @@
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import augmentationsData from "../assets/augmentations.json";
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import augmentationsData from '../assets/augmentations.json';
 
-import BodyPart from "./BodyPart";
+import BodyPart from './BodyPart';
 
 const partDetails = {
   head: {
-    position: { isLeft: true, top: "6%", left: "47.1%" },
+    position: { isLeft: true, top: '4%', left: '47.1%' },
     zoomPosition: { scale: 2.5, x: -400, y: 900 },
   },
   hand: {
-    position: { top: "18%", left: "31%" },
+    position: { top: '18%', left: '31%' },
     zoomPosition: { scale: 2.5, x: 1350, y: 700 },
   },
   uppertorso: {
-    position: { top: "21%", left: "44.6%" },
+    position: { top: '21%', left: '44.6%' },
     zoomPosition: { scale: 2, x: -500, y: 500 },
   },
   lowertorso: {
-    position: { top: "35%", left: "44.6%" },
+    position: { top: '35%', left: '44.6%' },
     zoomPosition: { scale: 2, x: -500, y: 100 },
   },
   leg: {
-    position: { isLeft: true, top: "50%", left: "44.6%" },
+    position: { isLeft: true, top: '50%', left: '44.6%' },
     zoomPosition: { scale: 2.2, x: 750, y: -100 },
   },
   feet: {
-    position: { isLeft: true, top: "85%", left: "44.6%" },
+    position: { isLeft: true, top: '85%', left: '44.6%' },
     zoomPosition: { scale: 2.5, x: 750, y: -900 },
   },
   arm: {
-    position: { top: "22%", left: "55%" },
+    position: { top: '22%', left: '55%' },
     zoomPosition: { scale: 2.5, x: -1350, y: 600 },
   },
 };
@@ -39,7 +39,7 @@ const BodyMap = () => {
   const [zoomedPart, setZoomedPart] = useState(null);
   const [shopItems, setShopItems] = useState([]);
   const [hiddenParts, setHiddenParts] = useState({});
-  const [shopPosition, setShopPosition] = useState("right");
+  const [shopPosition, setShopPosition] = useState('right');
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleClick = (part) => {
@@ -48,7 +48,7 @@ const BodyMap = () => {
     setHiddenParts((prev) => ({ ...prev, [part]: true }));
 
     // Determine if the shop should appear on the left or right
-    setShopPosition(partDetails[part].position.isLeft ? "left" : "right");
+    setShopPosition(partDetails[part].position.isLeft ? 'left' : 'right');
     setCurrentIndex(0); // Reset pagination when opening a new shop
   };
 
@@ -58,16 +58,12 @@ const BodyMap = () => {
   };
 
   return (
-    <div className="bodyContainer">
+    <div className='bodyContainer'>
       <motion.img
-        className="bodyImage"
-        src="/assets/statue.png"
-        alt="Body"
-        animate={
-          zoomedPart
-            ? partDetails[zoomedPart].zoomPosition
-            : { scale: 1, x: 0, y: 0 }
-        }
+        className='bodyImage'
+        src='/assets/statue.png'
+        alt='Body'
+        /*animate={zoomedPart ? partDetails[zoomedPart].zoomPosition : { scale: 1, x: 0, y: 0 }}*/
         transition={{ duration: 0.1, ease: [0.42, 0, 0.58, 1] }}
       />
 
@@ -76,12 +72,12 @@ const BodyMap = () => {
         (partKey) =>
           !hiddenParts[partKey] && (
             <motion.img
-              className="clickableImage"
-              src="/assets/Select-Box.png"
+              className='clickableImage'
+              src='/assets/Select-Box.png'
               style={{
                 top: partDetails[partKey].position.top,
                 left: partDetails[partKey].position.left,
-                width: "5%",
+                width: '5%',
                 opacity: zoomedPart ? 0 : 1,
               }}
               onClick={() => handleClick(partKey)}
@@ -90,14 +86,14 @@ const BodyMap = () => {
       )}
 
       {zoomedPart && (
-        <motion.div className="shopContainer" position={shopPosition}>
-          <button className="closeBtn" onClick={closeShop}>
+        <motion.div className='shopContainer' position={shopPosition}>
+          <button className='closeBtn' onClick={closeShop}>
             X
           </button>
-          <h2 className="title">{zoomedPart.toUpperCase()} AUGMENTATIONS</h2>
+          <h2 className='title'>{zoomedPart.toUpperCase()} AUGMENTATIONS</h2>
 
           {/* Scroll Effect */}
-          <div className="scrollContainer">
+          <div className='scrollContainer'>
             {shopItems.map((item, index) => {
               return <BodyPart key={index} item={item} />;
             })}
